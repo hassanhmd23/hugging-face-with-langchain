37e660cea4539da90e7069a5c405cf7fa709d77e
hassanhmd23 <hassanhmd1734@gmail.com>
2024-06-16 16:15:47 +0300

    removed audio

--- app.py
+++ app.py
@@ -17,15 +17,6 @@ def image_to_text(image: str) -> str:
     return response.json()[0]['generated_text']
 
 
-def text_to_speech(text: str) -> bytes:
-    api_url = "https://api-inference.huggingface.co/models/espnet/kan-bayashi_ljspeech_vits"
-    headers = {"Authorization": f'Bearer {os.environ.get("HUGGING_FACE_TOKEN")}'}
-
-    response = requests.post(api_url, headers=headers, json={"inputs": text, "options": {"wait_for_model": True}})
-    with open('audio.flac', 'wb') as file:
-        file.write(response.content)
-
-
 def main():
     st.set_page_config(page_title="img to audio description", page_icon="🤖")
 
@@ -38,12 +29,9 @@ def main():
             file.write(bytes_data)
         st.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
         description = image_to_text(uploaded_file.name)
-        text_to_speech(description)
 
         with st.expander("description"):
             st.write(description)
-
-        st.audio("audio.flac")
 
 
 if __name__ == "__main__":
